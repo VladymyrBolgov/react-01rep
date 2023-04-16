66cb846712a9036bcc6be8c220f9a814a2421e63
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-04-17 00:32:38 +0300

    src

---
+++ src/components/Painting.js
@@ -0,0 +1,23 @@
+const Painting = ({
+    imageUrl,
+    title,
+    profileUrl,
+    author = 'не известно',
+    price,
+}) => {
+  
+    return (
+      <div>
+        <img src={imageUrl} alt={title} width="480" />
+        <h2>{title}</h2>
+        <p>
+            Автор: <a href={profileUrl}>{author}</a>
+        </p>
+        <p>Цена: {price} кредитов</p>
+        <p>Доступность: заканчивается или есть в наличии</p>
+        <button type="button">Добавить в корзину</button>
+      </div>
+    );
+}
+
+export default Painting;
