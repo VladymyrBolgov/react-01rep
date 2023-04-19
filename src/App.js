404b02fdf70c3d968394623b152209e9285858ad
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-04-19 23:06:54 +0300

    app

--- src/App.js
+++ src/App.js
@@ -5,14 +5,7 @@ import paintings from './paintings.json'
 export default function App() {
     return (
         <div> 
-            {/* <PaintingList items={paintings} /> */}
-            <Section title="Топ недели">
-            <PaintingList items={paintings} />
-            </Section>
-
-            <Section title="Лучшее">
-            <PaintingList items={paintings} />
-            </Section>
+        
         </div> 
     )
 }
